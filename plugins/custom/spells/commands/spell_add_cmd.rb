2fcb9a90ce1aab9a7d793e5687121e9cf17a05dd
spiritlake <32268357+spiritlake@users.noreply.github.com>
2018-09-24 16:50:14 -0400

    Better error on spell/add

--- plugins/custom/spells/commands/spell_add_cmd.rb
+++ plugins/custom/spells/commands/spell_add_cmd.rb
@@ -13,12 +13,12 @@ module AresMUSH
         self.school = Global.read_config("spells", self.spell, "school")
       end
 
-      # def check_can_set
-      #   return t('dispatcher.not_allowed') if !FS3Skills.can_manage_abilities?(enactor)
-      # end
+      def check_can_set
+        return t('dispatcher.not_allowed') if !FS3Skills.can_manage_abilities?(enactor)
+      end
 
       def check_errors
-        return t('db.object_not_found') if !self.target
+        return t('custom.invalid_name') if !self.target
         return t('custom.not_spell') if !Custom.is_spell?(self.spell)
         if self.target.groups.values.include? self.school
           return nil
