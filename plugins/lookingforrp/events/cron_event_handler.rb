4d56b8f3bec6658583ff9d066af356a2f9956eaa
Nicole Arnold <aerrin99@gmail.com>
2024-12-04 15:29:09 -0500

    cron works

--- plugins/lookingforrp/events/cron_event_handler.rb
+++ plugins/lookingforrp/events/cron_event_handler.rb
@@ -7,14 +7,12 @@ module AresMUSH
        if Cron.is_cron_match?(looking_for_rp_cron, event.time)
           Global.logger.debug "Expiring Looking for RP flags."
 
-          chars = Chargen.approved_chars.select { |c| c.looking_for_rp_expires_at > Time.now }
-          puts "TEST"
-          puts chars.to_a
+          chars = LookingForRp.chars_looking_for_rp.select { |c| c.looking_for_rp_expires_at < Time.now }
+
           chars.each do |c|
             Global.logger.debug "Expiring Looking for RP flag for #{c.name}"
             LookingForRp.expire(c)
           end
-
         end
       end
 
