ad46622af1ad8362d58929b51d8bb05c2afae8e6
Nicole Arnold <aerrin99@gmail.com>
2024-12-05 15:54:30 -0500

    style sidebar, turn off looking for rp

--- plugins/lookingforrp/commands/looking_for_rp_cmd.rb
+++ plugins/lookingforrp/commands/looking_for_rp_cmd.rb
@@ -6,15 +6,22 @@ module AresMUSH
       attr_accessor :duration
 
       def parse_args
-        self.duration = cmd.args || 1
+        self.duration = cmd.args == 'off' ? nil : (cmd.args || 1)
       end
 
       def check_errors
+        return
         return "You can't set yourself 'Looking for RP' for longer than 3 hours." if duration.to_i > 3
       end
+
       def handle
-        LookingForRp.set(enactor, self.duration.to_i)
-        client.emit_success t('lookingforrp.set', :duration => self.duration)
+        if self.duration.nil?
+          LookingForRp.expire(enactor)
+          client.emit_success t('lookingforrp.expire')
+        else
+          LookingForRp.set(enactor, self.duration.to_i)
+          client.emit_success t('lookingforrp.set', :duration => self.duration)
+        end
       end
     end
   end
--- plugins/lookingforrp/help/looking_for_rp.md
+++ plugins/lookingforrp/help/looking_for_rp.md
@@ -7,6 +7,9 @@ The 'looking for RP' flag is used to indicate that you are actively seeking a di
 
 By default, the 'looking for RP' flag expires after one hour. If you know you will be searching for RP longer, you can set it for 2 or 3 hours, after which point it will be automatically cleared.
 
+See who is looking for RP on the webportal or by using `who` or `where`.
+
 ## Commands
 `lookingforrp` - Set yourself as looking for RP.
 `lookingforrp <2 or 3>` - Set yourself as looking for RP for a given number of hours.
+`lookingforrp off` - Turn off your looking for RP flag.
